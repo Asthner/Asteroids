a35df0e502b1131f49e666af289b11741fd95cc9
Asthner <asthner@gmail.com>
2025-01-22 23:53:08 +0100

    asteroids!

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame, player
+import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,13 +15,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    player.Player.containers = (updatable, drawable)
-    player1 = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player.containers = (updatable, drawable)
     
-
-
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
